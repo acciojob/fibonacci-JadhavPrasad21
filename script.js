675840dd1501da57cdaa95b57f209f2a70fe6ee1
Prasad Jadhav <86917788+JadhavPrasad21@users.noreply.github.com>
2023-07-19 08:53:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 function fibonacci(num) {
 // your code here
+
+	 if (num === 0) return 0;
+  if (num === 1) return 1;
+
+  let prev = 0;
+  let current = 1;
+  let next;
+
+  for (let i = 2; i <= num; i++) {
+    next = prev + current;
+    prev = current;
+    current = next;
+  }
+
+  return current;
+}
 }
 
 module.exports = fibonacci;
